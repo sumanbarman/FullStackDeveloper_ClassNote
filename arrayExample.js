2637ce12318033485db2b42f9a34d7a71e2a0fab
Barman <suman.barman@capgemini.com>
2018-03-10 12:30:35 +0530

    Modified array Example

--- arrayExample.js
+++ arrayExample.js
@@ -1,15 +1,20 @@
 var numlist = [10,20,30,40,50,60,70,80,90];
 var list = [10,20,'Suman Barman', 992];
 
+//Traditional Approach
 for(var i=0; i<numlist.length; i++){
     console.log(numlist[i]);
 }
 
+// For In feature
 for(let a in numlist){
     console.log(numlist[a]);
 }
 
-//Similar to Lamda
+//Fat Arrow - Multi Line
 numlist.forEach(element=>{
     console.log(element);
 })
+
+//Fat Arrow in Single line
+numlist.forEach(element=>console.log(element));
